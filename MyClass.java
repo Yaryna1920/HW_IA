300ff062724fa626856d22cab7093bd11f9ee2c6
iryna.antonian <irynantnn@gmail.com>
2023-11-19 19:59:43 +0200

    My Home Work 1

--- MyClass.java
+++ MyClass.java
@@ -1,2 +1,11 @@
 public class MyClass {
+
+    public static int sumOfTwoNumbers(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        sumOfTwoNumbers(2,3);
+    }
 }

8a514924ec307bee55cc2e4b11055fe63573af60
iryna.antonian <irynantnn@gmail.com>
2023-11-25 22:37:04 +0200

    My Home Work 1 commit 2

--- MyClass.java
+++ MyClass.java
@@ -1,11 +1,10 @@
 public class MyClass {
-
     public static int sumOfTwoNumbers(int a, int b) {
         int c = a + b;
         return c;
     }
 
     public static void main(String[] args) {
-        sumOfTwoNumbers(2,3);
+        sumOfTwoNumbers(2, 3);
     }
 }

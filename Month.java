dba61de914330e8cd57138636baecd9056b09ce4
iryna.antonian <irynantnn@gmail.com>
2023-12-02 23:38:32 +0200

    Commit 2 to HW3

--- Month.java
+++ Month.java
@@ -1,6 +1,6 @@
 public class Month {
     public static void main(String[] args) {
-        int monthNumber = 11;
+        int monthNumber = 12;
         printNumberOfDays(monthNumber);
     }
 
@@ -9,46 +9,27 @@ public class Month {
         int numberOfDays;
         switch (monthNumber) {
             case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
                 numberOfDays = 31;
                 break;
             case 2:
                 numberOfDays = 28;
                 break;
-            case 3:
-                numberOfDays = 31;
-                break;
             case 4:
-                numberOfDays = 30;
-                break;
-            case 5:
-                numberOfDays = 31;
-                break;
             case 6:
-                numberOfDays = 30;
-                break;
-            case 7:
-                numberOfDays = 31;
-                break;
-            case 8:
-                numberOfDays = 31;
-                break;
             case 9:
-                numberOfDays = 30;
-                break;
-            case 10:
-                numberOfDays = 31;
-                break;
             case 11:
                 numberOfDays = 30;
-                break;
-            case 12:
-                numberOfDays = 31;
                 break;
             default:
                 System.out.println("Wrong month number!");
                 return;
         }
-
         System.out.println("Number of days in current month: " + numberOfDays);
     }
 }
